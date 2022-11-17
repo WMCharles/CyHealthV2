9300c62ca2751d2ab61b7f810c5d6a14b5a2a1be
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-17 13:13:23 +0300

    add react router dom && font-awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import Statistics from './components/Statistics';
-import Loading from './components/Loading';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+import Stats from './components/Stats';
+import React from 'react'
+import './App.css'
 
 export default function App() {
-
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
-
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '9d8eb4a4c2mshea1727c847e873bp124a0fjsn0e91deaa1675',
-            'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
-        }
-    };
-
-    useEffect(() => {
-        fetch('https://covid-193.p.rapidapi.com/statistics', options)
-            .then(response => response.json())
-            .then((response) => {
-                setData(response.response)
-                setLoading(false)
-            })
-    }, [])
-
     return (
         <div className='container'>
-            <h1>CyHealth</h1>
-            {loading ? <Loading /> : <Statistics data={data} />}
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path='/' element={<Stats/>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
