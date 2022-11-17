fd6d3304fe21077aa6ff9c2cf5cb70f0d7e79054
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-17 11:50:58 +0300

    add statisctics component

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Statistics({ data }) {
+    return (
+        <table className='table table-striped'>
+            <thead>
+                <tr>
+                    <th>Country</th>
+                    <th>Total Cases</th>
+                    <th>New Cases</th>
+                    <th>Total Deaths</th>
+                    <th>New Deaths</th>
+                    <th>Total Recovered</th>
+                    <th>Active Cases</th>
+                    <th>Total Tests</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item, index) =>
+                    <tr key={index}>
+                        <td>{item.country}</td>
+                        <td>{item.cases.total}</td>
+                        <td>{item.cases.new}</td>
+                        <td>{item.deaths.total}</td>
+                        <td>{item.deaths.new}</td>
+                        <td>{item.cases.recovered}</td>
+                        <td>{item.cases.active}</td>
+                        <td>{item.tests.total}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    )
+}
