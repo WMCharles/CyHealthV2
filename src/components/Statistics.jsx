578aa3d3f59503b13e39e886b32ebe21d30be098
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-17 13:14:32 +0300

    add and pass prop

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,34 +1,38 @@
 import React from 'react'
+import Search from './Search'
 
-export default function Statistics({ data }) {
+export default function Statistics({ data, setQuery}) {
     return (
-        <table className='table table-striped'>
-            <thead>
-                <tr>
-                    <th>Country</th>
-                    <th>Total Cases</th>
-                    <th>New Cases</th>
-                    <th>Total Deaths</th>
-                    <th>New Deaths</th>
-                    <th>Total Recovered</th>
-                    <th>Active Cases</th>
-                    <th>Total Tests</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item, index) =>
-                    <tr key={index}>
-                        <td>{item.country}</td>
-                        <td>{item.cases.total}</td>
-                        <td>{item.cases.new}</td>
-                        <td>{item.deaths.total}</td>
-                        <td>{item.deaths.new}</td>
-                        <td>{item.cases.recovered}</td>
-                        <td>{item.cases.active}</td>
-                        <td>{item.tests.total}</td>
+        <div>
+            <Search input={setQuery}/>
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Country</th>
+                        <th>Total Cases</th>
+                        <th>New Cases</th>
+                        <th>Total Deaths</th>
+                        <th>New Deaths</th>
+                        <th>Total Recovered</th>
+                        <th>Active Cases</th>
+                        <th>Total Tests</th>
                     </tr>
-                )}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {data.map((item, index) =>
+                        <tr key={index}>
+                            <td>{item.country}</td>
+                            <td>{item.cases.total}</td>
+                            <td>{item.cases.new}</td>
+                            <td>{item.deaths.total}</td>
+                            <td>{item.deaths.new}</td>
+                            <td>{item.cases.recovered}</td>
+                            <td>{item.cases.active}</td>
+                            <td>{item.tests.total}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
     )
 }
