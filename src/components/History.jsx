5b5e356e701497a80aafccd31ca341d71cce3562
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-19 14:08:51 +0300

    add filter by country or continent functionality

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -6,6 +6,9 @@ import Loading from './Loading';
 export default function History() {
 
   const [numbers, setNumbers] = useState([])
+  const [countries, setCountries] = useState([])
+  const [continent, setContinent] = useState(["All", "Africa", "North-America", "South-America", "Asia", "Europe"])
+  const [selectedCountry, setSelectedCountry] = useState("all")
   const [loading, setLoading] = useState(true)
 
   const options = {
@@ -16,15 +19,32 @@ export default function History() {
     }
   };
 
-  useEffect(() => {
-    fetch('https://covid-193.p.rapidapi.com/history?country=all&day=2022-11-17', options)
+  const fetchData = async () => {
+    fetch(`https://covid-193.p.rapidapi.com/history?country=${selectedCountry}&day=2022-11-18`, options)
       .then(response => response.json())
       .then(response => {
         setNumbers(response.response)
         setLoading(false)
       })
       .catch(err => console.error(err));
-  }, [])
+  }
+
+  const fetchCountries = async () => {
+    fetch('https://covid-193.p.rapidapi.com/countries', options)
+      .then(response => response.json())
+      .then(response => {
+        setCountries(response.response)
+      })
+      .catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    fetchCountries()
+    if (selectedCountry) {
+      setLoading(true)
+      fetchData()
+    }
+  }, [selectedCountry])
 
   const label = numbers.map((data) => data.time)
   const cases = numbers.map((data) => data.cases.new)
@@ -35,6 +55,21 @@ export default function History() {
   return (
     <div className='History'>
       <h1>Covid-19 Hourly History</h1>
+      <form>
+        <input type="date" className="text" placeholder='enter country you want to search' onChange={(e) => setSelectedCountry(e.target.value)} />
+        <label htmlFor="countries">Select Country</label>
+        <select name="" id="" onChange={(e) => setSelectedCountry(e.target.value)}>
+          {countries.map((country, index) =>
+            <option key={index} value={country}>{country}</option>
+          )}
+        </select>
+        <label htmlFor="countries">Select Continent</label>
+        <select name="" id="" onChange={(e) => setSelectedCountry(e.target.value)}>
+          {continent.map((country, index) =>
+            <option key={index} value={country}>{country}</option>
+          )}
+        </select>
+      </form>
       {loading ? <Loading /> : <Graph label={label} cases={cases} deaths={deaths} tests={tests} />}
     </div>
   )
